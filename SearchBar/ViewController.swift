40e07d556035d5c5333f846e3a80e08240beaac0
daoseng33 <lc.ray2011@gmail.com>
2017-08-06 15:10:10 +0800

    [Modify] Support Swift 3.0
    - Add gitignore

--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, INSSearchBarDelegate
 		self.view.backgroundColor = UIColor(red: 0.000, green: 0.418, blue: 0.673, alpha: 1.000)
 		
 		let descriptionLabel: UILabel = UILabel(frame: CGRect(x: 20.0, y: 20.0, width: self.view.bounds.size.width - 40.0, height: 20.0))
-		descriptionLabel.textColor = UIColor.whiteColor()
+		descriptionLabel.textColor = UIColor.white
 		descriptionLabel.font = UIFont(name: "AvenirNext-Regular", size: 16.0)
 		descriptionLabel.text = "Without delegate"
 		
@@ -32,7 +32,7 @@ class ViewController: UIViewController, INSSearchBarDelegate
 		self.view.addSubview(self.searchBarWithoutDelegate)
 		
 		let descriptionLabel2: UILabel = UILabel(frame: CGRect(x: 20.0, y: 120.0, width: self.view.bounds.size.width - 40.0, height: 20.0))
-		descriptionLabel2.textColor = UIColor.whiteColor()
+		descriptionLabel2.textColor = UIColor.white
 		descriptionLabel2.font = UIFont(name: "AvenirNext-Regular", size: 16.0)
 		descriptionLabel2.text = "With delegate"
 		
@@ -50,28 +50,28 @@ class ViewController: UIViewController, INSSearchBarDelegate
 		// Dispose of any resources that can be recreated.
 	}
 	
-	func destinationFrameForSearchBar(searchBar: INSSearchBar) -> CGRect
+	func destinationFrameForSearchBar(_ searchBar: INSSearchBar) -> CGRect
 	{
 		return CGRect(x: 20.0, y: 140.0, width: self.view.bounds.size.width - 40.0, height: 34.0)
 	}
 	
-	func searchBar(searchBar: INSSearchBar, willStartTransitioningToState destinationState: INSSearchBarState)
+	func searchBar(_ searchBar: INSSearchBar, willStartTransitioningToState destinationState: INSSearchBarState)
 	{
 		// Do whatever you deem necessary.
 	}
 	
-	func searchBar(searchBar: INSSearchBar, didEndTransitioningFromState previousState: INSSearchBarState)
+	func searchBar(_ searchBar: INSSearchBar, didEndTransitioningFromState previousState: INSSearchBarState)
 	{
 		// Do whatever you deem necessary.
 	}
 	
-	func searchBarDidTapReturn(searchBar: INSSearchBar)
+	func searchBarDidTapReturn(_ searchBar: INSSearchBar)
 	{
 		// Do whatever you deem necessary.
 		// Access the text from the search bar like searchBar.searchField.text	
 	}
 	
-	func searchBarTextDidChange(searchBar: INSSearchBar)
+	func searchBarTextDidChange(_ searchBar: INSSearchBar)
 	{
 		// Do whatever you deem necessary.
 		// Access the text from the search bar like searchBar.searchField.text
